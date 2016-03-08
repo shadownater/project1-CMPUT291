790da1f57dee83fffb6b0f63aad8a983ef6697d2
Colleen Venhuis <venhuis@cs.ualberta.ca>
2016-03-08 15:21:55 -0700

    Lab edits

--- AutoTrans.java
+++ AutoTrans.java
@@ -43,40 +43,66 @@ public void autoTransMenu() {
 }
 
 public void addTransaction() {
-  // check if vehicle is in system or not
-  // if not, end function, suggest go to VehicleReg
-  // else, update vehicle transaction info
-  // tables required: owner, auto_sale
 
-  // owner( owner_id, vehicle_id, is_primary_owner )
-  // - char(15), char(15), char(1) (y/n)
-  // auto_sale( transaction_id, seller_id, buyer_id, vehicle_id, s_date, price )
-  // - int, char(15), char(15), char(15), date, numberic(9,2)
-
-  // ask for input:
-  System.out.print("Please fill out the following details in the alotted amount of space:\n");
-
+  // Initialize values
   String prev_owner; // seller_id
   String new_owner;  // buyer_id
   String auto_sold;  // vehicle_id
   Integer cost;     // price
+
+  // Initialize use variables
+  int ID_LEN = 15;
   
-  System.out.println("(15 characters)              | Vehicle Id: ");
-  auto_sold = "10102020\n"; //scanner.nextLine().toLowerCase();
-  // If vehicle does not exist, do not create new one.
-  // instead instruct user to go do a vehicle registration and
-  // return later
+  // Begin asking user for input:
+  System.out.print("Please fill out the following details in the alotted amount of space:\n");
 
-  // If people don't exist, however, create a new person.
-  System.out.println("(15 characters)              | Seller Id: ");
-  prev_owner = "Arthur 990"; //scanner.nextLine().toLowerCase(); // no spaces
+  // Ask user for auto_sold/vehicle_id
+  // If vehicle does not exist (check) then tell user to
+	// a) try again, enter correctly
+	// b) go to VehicleReg and come back once the vehicle is registered
+  System.out.print("(15 characters)              | Vehicle Id: ");
+  auto_sold = scanner.nextLine().toLowerCase();
 
-  System.out.println("(15 characters)              | Buyer Id: ");
-  new_owner = "Felicia123\n"; //scanner.nextLine().toLowerCase();
+  // Ask user for buyer and seller id
+  // If buyer and/or user does not exist, go create one and come back
+  // Input is converted to lower case and allows no spacing
+  System.out.print("(15 characters)              | Seller Id: ");
+  prev_owner = scanner.nextLine().toLowerCase();
 
-  System.out.println("(9 digits, 2 decimal places) | Price of sale: ");
-  cost = Integer.parseInt("80000.90");//Integer.parseInt(scanner.nextLine());
+  System.out.print("(15 characters)              | Buyer Id: ");
+  new_owner = scanner.nextLine().toLowerCase();
 
+  // Ask user for the price of the vehicle
+  System.out.print("(9 digits, 2 decimal places) | Price of sale: $");
+  cost = Integer.parseInt(scanner.nextLine());
+
+  // Confirm Auto Transaction
+  System.out.print("Are you sure you want to make these changes? Y/N:\n" +
+	" - Vehicle sold: " + auto_sold + "\n - Seller id: " + prev_owner +
+	"\n - Buyer id: " + new_owner + "\n - Price of sale: " + cost + "\n");
+
+  String input = scanner.nextLine();
+
+  if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("N")){
+    switch(input.toLowerCase()){
+    case "y":
+      break;
+      
+    case "n":
+      autoTransMenu();
+      break;
+    }
+    
+  } else {
+    System.out.println("Invalid input!");
+  }
+
+  // Update database with information given
+  // Tables required: owner, auto_sale
+  // owner( owner_id, vehicle_id, is_primary_owner )
+  // - char(15), char(15), char(1) (y/n)
+  // auto_sale( transaction_id, seller_id, buyer_id, vehicle_id, s_date, price )
+  // - int, char(15), char(15), char(15), date, numberic(9,2)
 
   System.out.print("\nTransaction Complete\n");
 }
